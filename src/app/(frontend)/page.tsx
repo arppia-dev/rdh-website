ff7f770f1db3a0ae708d47db5d423164a59f588a
Alexis Sniffer <alexis.sniffer@gmail.com>
2025-03-29 21:54:36 -0500

    update

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -19,13 +19,12 @@ export default async function HomePage() {
 
   return (
     <>
-      {/* <HeaderContact />
+      <HeaderContact />
       <Hero />
       <AboutUs />
       <Solutions />
       <Methodology />
-      <CaseStudies /> */}
-      Home
+      <CaseStudies />
     </>
   )
 }
