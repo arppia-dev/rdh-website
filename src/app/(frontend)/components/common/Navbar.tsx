3bf78f4f7759bfd58175aecc239dc5ad3c704bc1
Alexis Sniffer <alexis.sniffer@gmail.com>
2025-03-29 22:16:27 -0500

    update

--- src/app/(frontend)/components/common/Navbar.tsx
+++ src/app/(frontend)/components/common/Navbar.tsx
@@ -17,9 +17,9 @@ export default async function Navbar() {
           key={item.id}
           className="relative flex cursor-pointer items-center gap-1 hover:text-amber-200"
         >
-          <Link href={typeof item.page !== 'string' ? `/pages/${item.page.slug}` : item.page}>
+          {/* <Link href={typeof item.page !== 'string' ? `/pages/${item.page.slug}` : item.page}>
             {typeof item.page !== 'string' ? item.page.title : item.page}
-          </Link>
+          </Link> */}
 
           {/* {item.page === 'Inicio' && (
             <div className="absolute top-full left-0 h-0.5 w-full bg-white"></div>
