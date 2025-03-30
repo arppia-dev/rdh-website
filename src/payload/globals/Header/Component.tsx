c2c0ba9cd6978d054c1e56d3a5feb7607a30945f
Alexis Sniffer <alexis.sniffer@gmail.com>
2025-03-30 02:15:53 -0500

    update

--- src/payload/globals/Header/Component.tsx
+++ src/payload/globals/Header/Component.tsx
@@ -1,9 +1,10 @@
 import type { Header } from '@/payload-types'
-import { getCachedGlobal } from '../../utils/getGlobals'
-import { HeaderClient } from './Component.client'
+//import { getCachedGlobal } from '../../utils/getGlobals'
+//import { HeaderClient } from './Component.client'
 
 export async function Header() {
-  const headerData: Header = await getCachedGlobal('header', 1)()
+  //const headerData: Header = await getCachedGlobal('header', 1)()
 
-  return <HeaderClient data={headerData} />
+  //return <HeaderClient data={headerData} />
+  return <h1>Header</h1>
 }
