71ad8452a77dc893096cd293a5e3d2f4f404883c
Alexis Sniffer <alexis.sniffer@gmail.com>
2025-03-29 21:53:29 -0500

    update

--- src/app/(frontend)/pages/[slug]/page.tsx
+++ src/app/(frontend)/pages/[slug]/page.tsx
@@ -1,37 +1,11 @@
 import { RenderBlocks } from '@/payload/blocks/RenderBlocks'
-import configPromise from '@payload-config'
-import { draftMode } from 'next/headers'
-import { getPayload, type RequiredDataFromCollectionSlug } from 'payload'
-import { cache } from 'react'
+import { queryPageBySlug } from '@/payload/utils/queryPageBySlug'
+import { type RequiredDataFromCollectionSlug } from 'payload'
 
 type PageProps = {
   params: Promise<{
     slug?: string
   }>
-}
-
-export async function generateStaticParams() {
-  const payload = await getPayload({ config: configPromise })
-  const pages = await payload.find({
-    collection: 'pages',
-    draft: false,
-    limit: 1000,
-    overrideAccess: false,
-    pagination: false,
-    select: {
-      slug: true,
-    },
-  })
-
-  const params = pages.docs
-    ?.filter((doc) => {
-      return doc.slug !== 'home'
-    })
-    .map(({ slug }) => {
-      return { slug }
-    })
-
-  return params
 }
 
 export default async function Page({ params: paramsPromise }: PageProps) {
@@ -47,24 +21,3 @@ export default async function Page({ params: paramsPromise }: PageProps) {
 
   return <RenderBlocks blocks={page.layout} />
 }
-
-const queryPageBySlug = cache(async ({ slug }: { slug: string }) => {
-  const { isEnabled: draft } = await draftMode()
-
-  const payload = await getPayload({ config: configPromise })
-
-  const result = await payload.find({
-    collection: 'pages',
-    draft,
-    limit: 1,
-    pagination: false,
-    overrideAccess: draft,
-    where: {
-      slug: {
-        equals: slug,
-      },
-    },
-  })
-
-  return result.docs?.[0] || null
-})
