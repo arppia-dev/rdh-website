66d2a39241b61f984f574433f84b7f0d255fe632
Alexis Sniffer <alexis.sniffer@gmail.com>
2025-03-29 22:23:50 -0500

    update

--- src/app/(frontend)/components/common/Navbar.tsx
+++ src/app/(frontend)/components/common/Navbar.tsx
@@ -1,31 +1,33 @@
-import { Page } from '@/payload-types'
+/*import { Page } from '@/payload-types'
 import configPromise from '@payload-config'
 import Link from 'next/link'
-import { getPayload } from 'payload'
+import { getPayload } from 'payload'*/
 
 export default async function Navbar() {
-  const payload = await getPayload({ config: configPromise })
+  /*const payload = await getPayload({ config: configPromise })
 
   const { items } = await payload.findGlobal({
     slug: 'nav',
-  })
+  })*/
 
+  return <h1>prueba</h1>
+  /*
   return (
+
+
     <nav className="hidden items-center gap-8 md:flex">
       {items.map((item: { page: string | Page; id?: string | null }) => (
         <div
           key={item.id}
           className="relative flex cursor-pointer items-center gap-1 hover:text-amber-200"
         >
-          {/* <Link href={typeof item.page !== 'string' ? `/pages/${item.page.slug}` : item.page}>
+           <Link href={typeof item.page !== 'string' ? `/pages/${item.page.slug}` : item.page}>
             {typeof item.page !== 'string' ? item.page.title : item.page}
-          </Link> */}
+          </Link>
 
-          {/* {item.page === 'Inicio' && (
-            <div className="absolute top-full left-0 h-0.5 w-full bg-white"></div>
-          )} */}
         </div>
       ))}
     </nav>
   )
+ */
 }
