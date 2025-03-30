ca1be00f381b40b48a4592a2b7daca10d32d35ae
Alexis Sniffer <alexis.sniffer@gmail.com>
2025-03-30 01:47:08 -0500

    update

--- src/payload/globals/Header/Component.tsx
+++ src/payload/globals/Header/Component.tsx
@@ -1,15 +1,16 @@
 import { type Header, Page } from '@/payload-types'
 import { getCachedGlobal } from '@/payload/utils/getGlobals'
-import Link from 'next/link'
+// import Link from 'next/link'
 import { JSX } from 'react'
 
 export async function Header(): Promise<JSX.Element | undefined> {
   const headerData: Header = await getCachedGlobal('header', 1)()
-  const navItems = headerData?.navItems || []
+  // const navItems = headerData?.navItems || []
 
   return (
     <nav className="hidden items-center gap-8 md:flex">
-      {navItems.map((item: { page: string | Page; id?: string | null }) => (
+      Hola
+      {/* {navItems.map((item: { page: string | Page; id?: string | null }) => (
         <div
           key={item.id}
           className="relative flex cursor-pointer items-center gap-1 hover:text-amber-200"
@@ -18,7 +19,7 @@ export async function Header(): Promise<JSX.Element | undefined> {
             {typeof item.page !== 'string' ? item.page.title : item.page}
           </Link>
         </div>
-      ))}
+      ))} */}
     </nav>
   )
 }
